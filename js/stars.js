c26762082888301fc6fa582a3cf1bf87dfc0244b
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2018-08-05 21:10:21 +0300

    Take class `stars` and put into it <div> elements with class `star` that is mean made nesting Now, all 500 stars are in one place and we can assignment it to one variable called `stars` and then manipulate with it. Next step arrange all stars on the screen

--- js/stars.js
+++ js/stars.js
@@ -1,4 +1,12 @@
 addEventListener(`load`, function () {
     function makeStars() {
+        for (let i = 0; i < 500; i++) {
+            //Создаем звезды путем увеличения количества div элементов c класом star
+            document.querySelector('.stars').innerHTML += '<div class="star"></div>';
+        }
+        //все новосозданые звезды выбираем и присваеваем переменной stars
+        let stars = document.querySelectorAll('.star');
 
+    }
+    makeStars();
 });
