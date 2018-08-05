2bb9c7acc78367df85e4f95157d43a06370e11d8
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2018-08-05 21:32:06 +0300

    Added little more describes for animation stars and some sense of several function

--- js/stars.js
+++ js/stars.js
@@ -1,4 +1,6 @@
+//Событие, что происходит при загрузке экрана браузера бляголаря тикому собитию как `load`
 addEventListener(`load`, function () {
+    //Функция что создает звезды и делает их живыми
     function makeStars() {
         for (let i = 0; i < 500; i++) {
             //Создаем звезды путем увеличения количества div элементов c класом star
@@ -17,8 +19,12 @@ addEventListener(`load`, function () {
             stars[j].style.height = size + "px";
             //Ширина звезды
             stars[j].style.width = size + "px";
-
+            //Делаем мерцание в случайной последовательности
+            let animate = (Math.random() * 2) + 1;
+            //Получаю доступ к стилям звезды и анимирую ее путем добавления стилей для мерцания через цикл что использовал выше
+            stars[j].style.animation = "twinkle " + animate + "s infinite";
         }
     }
+    //Вызываю функцию
     makeStars();
 });
