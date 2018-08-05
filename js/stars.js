93dafc0da34ba1e7bb8c0cc5dbe32c0d0403377f
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2018-08-05 21:18:47 +0300

    New loop through the `stars` variable there are 500 items, then fill out whole screen by stars from left to right and from top to bottom on 100% with randomize

--- js/stars.js
+++ js/stars.js
@@ -6,7 +6,13 @@ addEventListener(`load`, function () {
         }
         //все новосозданые звезды выбираем и присваеваем переменной stars
         let stars = document.querySelectorAll('.star');
+        //Умножаем звезды и рапологаем их по екрану
+        for (let j = 0; j < stars.length; j++) {
+            //Размещаем звезды с лева на право случайным образом. Область заполнения 100%
+            stars[j].style.left = (Math.random() * 100) + "%";
+            stars[j].style.top = (Math.random() * 100) + "%";
 
+        }
     }
     makeStars();
 });
