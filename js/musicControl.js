6141101c4bb031f2286f3e7e69e8aa30f5b28fb4
Maksym Gordiyenko <maxim.gordiyenko@gmail.com>
2019-05-12 01:07:49 +0300

    delete pluto

--- js/musicControl.js
+++ js/musicControl.js
@@ -1,7 +1,7 @@
 //Create constructor new with audio file
 let sound = new Audio('music/sky-view-sound.ogg');
 //Load music when page open
-window.addEventListener(`load`, function () {
+window.addEventListener(`load`,  () => {
     //built-in function play()
     sound.play(); //todo: resolve problem with Promise
 }, false);
